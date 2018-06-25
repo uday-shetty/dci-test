79ba8485a2e2dcf2e8209c7aef02d109b3e9cd99
uday-shetty <uday.shetty@docker.com>
2018-06-25 11:51:21 -0700

    download terraform and ansible scripts

--- dci-script.sh
+++ dci-script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y software-properties-common
 
-apt-get install -y terraform
-apt-get install -y ansible
+sudo apt-get install -y unzip
+wget https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip
+unzip terraform_0.11.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform --version
+
+sudo apt-get update
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get install -y ansible
+ansible --version
